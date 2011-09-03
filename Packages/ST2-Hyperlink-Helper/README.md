2fa803fe7c4c4b648db51122650ab42302e3384f
Mehdi Kabab <pioupioum@gmail.com>
2011-09-03 15:04:21 +0200

    Add Hyperlink Helper package.

---
+++ Packages/ST2-Hyperlink-Helper/README.md
@@ -0,0 +1,40 @@
+ST2 Hyperlink Helper
+====================
+
+a port of [TextMate’s][tm] Hyperlink Helper bundle for [Sublime Text 2][st2]
+
+What is this?
+-------------
+
+ST2 Hyperlink Helper is a collection of commands for the [Sublime Text 2][st2] text editor for Windows, OS X, and Linux. These commands make it super easy to insert hyperlinks into documents of various kinds using keyboard shortcuts.
+
+Currently supported languages:
+
+  * HTML
+  * Markdown
+  * Textile
+  * LaTeX
+  * DokuWiki (once Sublime Text 2 has a DokuWiki language file added to it)
+
+Currently offered commands:
+
+  * Lookup with Google & Link (Ctrl+Alt+Shift+L, ^⇧⌘L on Mac)
+  * Wrap Word / Selection as Link (Ctrl+Alt+L, ^⇧L on Mac)
+
+Why is this needed?
+-------------------
+
+Hyperlink Helper is one of the most popular features of [TextMate][tm] a popular text editor for the Mac. In many ways, [Sublime Text 2][st2] is a superior editor to TextMate, but it lacks many of the convenience features that make TextMate so popular. This project is an effort to bring some of those features to Sublime Text 2.
+
+How do I install this?
+----------------------
+
+Download the code for this project and drop it into a subdirectory of Sublime Text 2’s Packages folder. You can get to the Packages folder by choosing **Browse Packages…** from Sublime Text 2’s **Preferences** menu.
+
+Project Status
+--------------
+
+This project is currently alpha. Functionality is largely incomplete, but what’s there works to a useful standard.
+
+[st2]: http://www.sublimetext.com/2
+[tm]: http://macromates.com/
---
+++ Packages/ST2-Hyperlink-Helper/lookup_with_google_and_link.py
@@ -0,0 +1,51 @@
+import sublime, sublime_plugin
+import re, urllib, urllib2
+import os, sys
+import chardet, pystache
+
+def preemptive_imports():
+	""" needed to ensure ability to import these classes later within functions, due to the way ST2 loads plug-in modules """
+	from chardet import universaldetector
+preemptive_imports()
+
+class LookupWithGoogleAndLinkCommand(sublime_plugin.TextCommand):
+
+	def get_link_with_title(self, phrase):
+		try:
+			url = "http://www.google.com/search?%s&btnI=I'm+Feeling+Lucky" % urllib.urlencode({'q': phrase})
+			req = urllib2.Request(url, headers={'User-Agent' : "Sublime Text 2 Hyperlink Helper"}) 
+			f = urllib2.urlopen(req)
+			url = f.geturl()
+			content = f.read()
+			decoded_content = content.decode(chardet.detect(content)['encoding'])
+			title = re.search(r"<title>([^<>]*)</title>", decoded_content, re.I).group(1)
+			title = title.strip()
+			return url, title, phrase
+		except urllib2.URLError, e:
+			sublime.error_message("Error fetching Google search result: %s" % str(e))
+			return None
+
+	def run(self, edit):
+		nonempty_sels = []
+
+		# set aside nonempty selections
+		for s in reversed(self.view.sel()):
+			if not s.empty():
+				nonempty_sels.append(s)
+				self.view.sel().subtract(s)
+
+		# expand remaining (empty) selections to words
+		self.view.run_command("expand_selection", {"to": "word"})
+
+		# add nonempty selections back in
+		for s in nonempty_sels:
+			self.view.sel().add(s)
+		
+		# apply the links
+		for s in reversed(self.view.sel()):
+			if not s.empty():
+				txt = self.view.substr(s)
+				link = self.get_link_with_title(txt)
+				if not link:
+					continue
+				self.view.replace(edit, s, pystache.render(self.view.settings().get('hyperlink_helper_link_format'), {'url': link[0], 'title?': {'title': link[1]}, 'input': link[2]}))
